1cc07f0f8417353b9d46e9b8b03c810d87bcdf84
Aleksey Makarov <aleksey.makarov@gmail.com>
2018-09-19 11:04:50 +0300

    trifecta

--- trifecta.hs
+++ trifecta.hs
@@ -9,15 +9,15 @@ import           Text.Parser.Token
 import           Text.Trifecta.Parser
 import           Text.Trifecta.Result
 
-data LayoutLocation = LayoutLocation String deriving Show
+data LayoutLocation = LayoutLocation Integer deriving Show
 
 layoutLocationP :: (TokenParsing m, Errable m) => m LayoutLocation
-layoutLocationP = (LayoutLocation <$> brackets (some digit)) <?> "layout location"
+layoutLocationP = (LayoutLocation <$> brackets integer) <?> "layout location"
 
-data BitmapLocation = BitmapLocation String deriving Show
+data BitmapLocation = BitmapLocation Integer deriving Show
 
 bitmapLocationP :: (TokenParsing m, Errable m) => m BitmapLocation
-bitmapLocationP = (BitmapLocation <$> angles (some digit)) <?> "bitfield location"
+bitmapLocationP = (BitmapLocation <$> angles integer) <?> "bitfield location"
 
 data Name = Name String deriving Show
 
@@ -27,40 +27,44 @@ nameP = Name <$> (some $ satisfyRange 'A' 'Z') <?> "name of item"
 data Doc = Doc String deriving Show
 
 docP :: (TokenParsing m, Errable m) => m Doc
-docP = Doc <$> (stringLiteral <|> untilEOForBrace) <?> "documentation string"
-  where
-    untilEOForBrace = (runUnspaced $ many $ satisfy (\ c -> c /= '{' && c /= '\n'))
+docP = Doc <$> (whiteSpace *> stringLiteral) <?> "documentation string"
+-- docP = Doc <$> (whiteSpace *> (stringLiteral <|> untilEOLOrBrace)) <?> "documentation string"
+  -- where
+    -- untilEOLOrBrace =                many $ satisfy (\ c -> c /= '{')
+    -- untilEOLOrBrace =                many $ satisfy (\ c -> c /= '{' && c /= '\n')
+    -- untilEOLOrBrace = (runUnspaced $ many $ satisfy (\ c -> c /= '{' && c /= '\n'))
+    -- untilEOLOrBrace = (runUnlined  $ many $ satisfy (\ c -> c /= '{' && c /= '\n'))
 
-data ValueItem = ValueItem String Name Doc deriving Show
+data ValueItem = ValueItem Integer Name Doc deriving Show
 
 numberP :: (TokenParsing m, Errable m) => m String
 numberP = some digit <?> "number"
 
-valueItem :: (TokenParsing m, Errable m) => m ValueItem
-valueItem = (char '=' *> (ValueItem <$> numberP <*> nameP <*> docP)) <?> "value item"
+valueItemP :: (TokenParsing m, Errable m) => m ValueItem
+valueItemP = (char '=' *> (ValueItem <$> integer <*> nameP <*> docP)) <?> "value item"
 
 type BitmapBody = [Either ValueItem BitmapItem]
 
 bitmapBodyP :: (TokenParsing m, Errable m) => m BitmapBody
-bitmapBodyP = undefined
+bitmapBodyP = some (Left <$> valueItemP <|> Right <$> bitmapItemP)
 
 data BitmapItem = BitmapItem BitmapLocation Name Doc (Maybe BitmapBody) deriving Show
 
-bitmapItem :: (TokenParsing m, Errable m) => m BitmapItem
-bitmapItem = (BitmapItem <$> bitmapLocationP <*> nameP <*> docP <*> optional (braces bitmapBodyP)) <?> "bitmap item"
+bitmapItemP :: (TokenParsing m, Errable m) => m BitmapItem
+bitmapItemP = (BitmapItem <$> bitmapLocationP <*> nameP <*> docP <*> optional (braces bitmapBodyP)) <?> "bitmap item"
 
 type LayoutBody = Either [LayoutItem] BitmapBody
 
 layoutBodyP :: (TokenParsing m, Errable m) => m LayoutBody
-layoutBodyP = undefined
+layoutBodyP = Left <$> (some layoutItemP) <|> Right <$> bitmapBodyP
 
 data LayoutItem = LayoutItem LayoutLocation Name Doc (Maybe LayoutBody) deriving Show
 
-layoutItem :: (TokenParsing m, Errable m) => m LayoutItem
-layoutItem = (LayoutItem <$> layoutLocationP <*> nameP <*> docP <*> optional (braces layoutBodyP)) <?> "layout item"
+layoutItemP :: (TokenParsing m, Errable m) => m LayoutItem
+layoutItemP = (LayoutItem <$> layoutLocationP <*> nameP <*> docP <*> optional (braces layoutBodyP)) <?> "layout item"
 
 parser :: (TokenParsing m, Errable m) => m [LayoutItem]
-parser = whiteSpace *> (some layoutItem) <* eof
+parser = whiteSpace *> (some layoutItemP) <* eof
 
 main :: IO ()
 main = do
