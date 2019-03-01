affd6d1603b4f40000748aae824af695722e7c22
Aleksey Makarov <aleksey.makarov@gmail.com>
2019-03-01 10:54:56 +0300

    rewrite test

--- test/Main.hs
+++ test/Main.hs
@@ -17,55 +17,60 @@ import           Text.Trifecta.Parser
 import           Text.Trifecta.Result
 import           Turtle hiding (f, x, e)
 
-ifM :: Monad m => m Bool -> m a -> m a -> m a
-ifM b t f = do b' <- b; if b' then t else f
-
 putDocFile :: String -> Doc ann -> IO ()
 putDocFile pathString doc = withFile pathString WriteMode putDocFile'
-  where
-    putDocFile' :: Handle -> IO ()
-    putDocFile' h = hPutDoc h doc
+    where
+        putDocFile' :: Handle -> IO ()
+        putDocFile' h = hPutDoc h doc
 
 putErrInfoFile :: String -> ErrInfo -> IO ()
 putErrInfoFile pathString e = withFile pathString WriteMode putErrInfoFile'
-  where
-    doc = _errDoc e
-    -- FIXME: don't print formatting symbols into the error file
-    putErrInfoFile' h = PAL.hPutDoc h doc
-
-mkOut :: String -> IO ()
-mkOut pathString = parseFromFileEx parser pathString >>= foldResult (putErrInfoFile errPathString) mkOutOk
     where
-      mkOutOk x = putDocFile outPathString $ vcat $ fmap pretty x
-      outPathString = pathString ++ ".out"
-      errPathString = pathString ++ ".err"
+        doc = _errDoc e
+        -- FIXME: don't print formatting symbols into the error file
+        putErrInfoFile' h = PAL.hPutDoc h doc
 
-mkGold :: FilePath -> Shell TestTree
-mkGold path = do
-  rmIfExists $ path <.> "out"
-  rmIfExists $ path <.> "err"
-  return $ goldenVsFile testName goldenString outString (mkOut pathString)
-  where
-    testName = (encodeString $ basename path) ++ " (G)"
-    pathString = encodeString path
-    outString = encodeString $ path <.> "out"
-    goldenString = encodeString $ path <.> "gold"
-    rmIfExists p = ifM (testfile p) (rm p) (return ())
+rmIfExists :: FilePath -> Shell ()
+rmIfExists p = do
+    b <- testfile p
+    when b $ rm p
 
 makeTestCase :: FilePath -> Shell TestTree
 makeTestCase path =
     if path `hasExtension` "mlayout"
-        then ifM hasGold (mkGold path) $ if (dropExtension path) `hasExtension` "err"
-            then return mkTestCaseErr
-            else return mkTestCaseOk
+        then do
+          rmIfExists prettyPath
+          rmIfExists errPath
+          return $ if (dropExtension path) `hasExtension` "err"
+            then mkErr
+            else mkGoldPretty
         else mzero
     where
-        testName = encodeString $ basename path
         pathString = encodeString path
-        mkTestCase name ok failure = testCase name $ parseFromFileEx parser pathString >>= foldResult (const failure) (const ok)
-        mkTestCaseOk = mkTestCase testName (return ()) (assertFailure "should pass")
-        mkTestCaseErr = mkTestCase (testName ++ " (E)") (assertFailure "should fail") (return ())
-        hasGold = testfile (path <.> "gold")
+
+        testPrettyName = (encodeString $ basename path) ++ " (pretty)"
+        testErrorName = (encodeString $ basename path) ++ " (error)"
+
+        prettyPath = path <.> "pretty"
+        prettyPathString = encodeString prettyPath
+        prettyGoldPathString = encodeString $ path <.> "pretty" <.> "gold"
+        errPath = path <.> "err"
+        errPathString = encodeString $ errPath
+
+        mkOutOk :: Pretty d => [d] -> IO ()
+        mkOutOk doc = putDocFile prettyPathString $ vcat $ fmap pretty doc
+
+        mkOutPretty :: IO ()
+        mkOutPretty = parseFromFileEx parser pathString >>= foldResult (putErrInfoFile errPathString) mkOutOk
+
+        mkGoldPretty :: TestTree
+        mkGoldPretty = goldenVsFile testPrettyName prettyGoldPathString prettyPathString mkOutPretty
+
+        mkErr :: TestTree
+        mkErr = testCase testErrorName $ do
+            mkOutPretty
+            b <- testfile errPath
+            unless b $ assertFailure "should fail"
 
 main :: IO ()
 main = do
