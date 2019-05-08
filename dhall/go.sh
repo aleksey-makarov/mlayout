55e5e4136efb888412ede9864c297a3b089611ba
Aleksey Makarov <aleksey.makarov@gmail.com>
2019-05-08 13:18:31 +0300

    dhall: go.sh

--- dhall/go.sh
+++ dhall/go.sh
@@ -1,4 +1,14 @@
-src=tree.dhall
+# set -x
+
+if [ -z $1 ] ; then
+    src=tree.dhall
+else
+    cd $(dirname "$1")
+    src=$(basename "$1")
+fi
+
+# echo "\"$src\""
 
 dhall format --inplace $src
+# dhall lint --inplace $src
 dhall --explain < $src
