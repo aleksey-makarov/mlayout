609349a7741b5e66f7b8555cd528d91eb0e80c85
Aleksey Makarov <aleksey.makarov@gmail.com>
2019-06-26 10:44:39 +0300

    xtree: wip

--- dhall/XTree.hs
+++ dhall/XTree.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveTraversable #-}
 
 module XTree where
 
 import Data.Functor.Foldable
 
-data XTreeBase l n a = XTreeBase [Either l (n, a)] deriving Functor
+data XTreeBase l n a = XTreeBase [Either l (n, a)] deriving (Functor, Traversable, Foldable)
 
 type XTree l n = Fix (XTreeBase l n)
--- dhall/xtree.hs
+++ dhall/xtree.hs
@@ -1,20 +1,60 @@
 #!/usr/bin/env stack
 {- stack script --resolver nightly-2019-06-21
     --package dhall
+    --package directory
+    --package prettyprinter
     --package recursion-schemes
     --package text
 -}
 
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+import Control.Monad
+import Data.Bool
+import Data.Functor.Foldable
 import Data.Text
+import Data.Text.Prettyprint.Doc
+import System.Directory
 
 import XTree
 
-type File = Text
-type Directory = Text
+type File = FilePath
+type Directory = FilePath
 type DirectoryTree = XTree File Directory
 
+-- | A monadic catamorphism
+cataM
+  :: (Recursive t, Traversable (Base t), Monad m)
+  => (Base t a -> m a) -- ^ a monadic (Base t)-algebra
+  -> t                 -- ^ fixed point
+  -> m a               -- ^ result
+cataM f = c where c = f <=< mapM c <=< (return . project)
+
+-- | A monadic anamorphism
+anaM
+  :: (Corecursive t, Traversable (Base t), Monad m)
+  => (a -> m (Base t a))        -- ^ a monadic (Base t)-coalgebra
+  -> a                          -- ^ seed
+  -> m t
+anaM g = a where a = (return . embed) <=< mapM a <=< g
+
+mkDirTreeCoalg :: FilePath -> IO (XTreeBase File Directory FilePath)
+mkDirTreeCoalg p = XTreeBase <$> (mapM f =<< listDirectory p)
+        where
+            f :: FilePath -> IO (Either File (Directory, FilePath))
+            f p = bool (Right (p, p)) (Left p) <$> doesDirectoryExist p
+
 mkDirTree :: FilePath -> IO DirectoryTree
-mkDirTree = undefined
+mkDirTree = anaM mkDirTreeCoalg
+
+prettyDirectoryTree :: XTreeBase File Directory (Doc ana) -> Doc ana
+prettyDirectoryTree = undefined
+
+instance Pretty DirectoryTree where
+    pretty = cata prettyDirectoryTree
 
 main = do
     putStrLn "Hello world"
