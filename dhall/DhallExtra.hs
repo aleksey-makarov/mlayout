f9e9dc6340871bf514df494c0e86607379df1cf5
Aleksey Makarov <aleksey.makarov@gmail.com>
2019-06-29 13:24:39 +0300

    simplify Inject Tree implementation

--- dhall/DhallExtra.hs
+++ dhall/DhallExtra.hs
@@ -14,35 +14,28 @@ import Dhall.Map as DM
 import Dhall.Parser
 import Dhall.TypeCheck
 
---    → λ(a : Type)
---    → λ(f : { data : dtype, subtrees : List a } → a)
---    → f { data = d, subtrees = List/map (Tree dtype) a (λ(tree : Tree dtype) → tree a f) children }
+treeType = $(staticDhallExpression "./Tree/Type")
+treeToDhallExpression = $(staticDhallExpression "let Tree = ./Tree/Type \
+                                                \let List/map = ./List/map \
+                                                \in λ(dtype : Type) \
+                                                \ → λ(d : dtype) \
+                                                \ → λ(children : List (Tree dtype)) \
+                                                \ → λ(a : Type) \
+                                                \ → λ(f : { data : dtype, subtrees : List a } → a) \
+                                                \ → f { data = d, subtrees = List/map (Tree dtype) a (λ(tree : Tree dtype) → tree a f) children }"
+                        )
 
 treeToDhall :: Expr Src X -> Expr Src X -> [Expr Src X] -> Expr Src X
-treeToDhall dtype d children =
-                    Lam "a" (Const DC.Type) $
-                    Lam "f" (Pi "_" (Record $ DM.fromList [("data", dtype), ("subtrees", App List (v "a"))]) (v "a")) $
-                    App (v "f") (RecordLit $ DM.fromList [("data", d), ("subtrees", s)])
+treeToDhall dtype d children = App (App (App treeToDhallExpression dtype) d) childrenExpression
   where
-    v n = Var (V n 0)
-    f   = Lam "tree" (App (v "Tree") dtype) (App (App (v "tree") (v "a")) (v "f"))
-    s   = App (App (App (App (v "List/map") (App (v "Tree") dtype)) (v "a")) f) c'
-    c'  = ListLit (Just (App (v "Tree") dtype)) (DS.fromList children)
-
-appendLets :: Expr Src X -> Expr Src X
-appendLets e = Let (LNE.fromList [Binding "Tree" Nothing treeType, Binding "List/map" Nothing listMap]) e
-  where
-    treeType = $(staticDhallExpression "./Tree/Type")
-    listMap = $(staticDhallExpression "./List/map")
+    childrenExpression  = ListLit (Just (App treeType dtype)) (DS.fromList children)
 
 instance Inject d => Inject (Tree d) where
     injectWith options = InputType {..}
       where
-        embed = normalize . appendLets . embed'
+        embed = normalize . embed'
         embed' (Node d ns) = treeToDhall declaredIn (embedIn d) (fmap embed' ns)
-        -- ∀(a : Type) → ({ data : t, subtrees : List a } -> a) -> a
-        declared = Pi "a" (Const DC.Type) $ Pi "_" (Pi "_" (Record $ DM.fromList [("data", declaredIn), ("subtrees", (App List (v "a")))]) (v "a")) (v "a")
+        declared = normalize $ App treeType declaredIn
         InputType embedIn declaredIn = injectWith options
-        v n = Var (V n 0)
 
 
