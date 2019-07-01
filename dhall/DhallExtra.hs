cf18cc2c7dcadc95f604a904a98eea615c0be9fa
Aleksey Makarov <aleksey.makarov@gmail.com>
2019-07-01 09:54:48 +0300

    xtree: Inject XTree

--- dhall/DhallExtra.hs
+++ dhall/DhallExtra.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -17,6 +19,7 @@ import Dhall.Parser
 import Dhall.TypeCheck
 
 import DataFunctorFoldableExtra
+import XTree
 
 treeType :: Expr Src X
 treeType = $(staticDhallExpression "./Tree/Type")
@@ -43,4 +46,45 @@ instance Inject d => Inject (Tree d) where
         declared = normalize $ App treeType declaredIn
         InputType embedIn declaredIn = injectWith options
 
+xtreeType :: Expr Src X
+xtreeType = $(staticDhallExpression "./XTree/Type")
 
+xtreeEmbed :: Expr Src X
+xtreeEmbed = $(staticDhallExpression "let XTree = ./XTree/Type \
+                                     \in λ(ltype : Type) \
+                                     \ → λ(ntype : Type) \
+                                     \ → λ(children : List < leaf : ltype | node : { node : ntype, subtrees : XTree ltype ntype } >) \
+                                     \ → ((./XTree/steppable ltype ntype).embed children)"
+              )
+
+xtreeToDhall :: Expr Src X -> Expr Src X -> [Expr Src X] -> Expr Src X
+xtreeToDhall ltype ntype children = App (App (App xtreeEmbed ltype) ntype) (listToDhallList (App (App xtreeType ltype) ntype) children)
+
+mkLeaf :: Expr Src X
+mkLeaf = $(staticDhallExpression "let XTree = ./XTree/Type \
+                                 \in λ(ltype : Type) \
+                                 \ → λ(ntype : Type) \
+                                 \ → λ(l : ltype) \
+                                 \ → < leaf : ltype | node : { node : ntype, subtrees : XTree ltype ntype } >.leaf l"
+          )
+
+mkNode :: Expr Src X
+mkNode = $(staticDhallExpression "let XTree = ./XTree/Type \
+                                 \in λ(ltype : Type) \
+                                 \ → λ(ntype : Type) \
+                                 \ → λ(n : ntype) \
+                                 \ → λ(t : XTree ltype ntype) \
+                                 \ → < leaf : ltype | node : { node : ntype, subtrees : XTree ltype ntype } >.node { node = n, subtrees = t }"
+          )
+
+
+instance (Inject l, Inject n) => Inject (XTree l n) where
+    injectWith options = InputType {..}
+      where
+        embed = normalize . cata embedAlg
+        embedAlg (XTreeF l) = xtreeToDhall declaredL declaredN (eitherToDhall <$> l)
+        declared = normalize $ App (App xtreeType declaredL) declaredN
+        eitherToDhall (Left l) = App (App (App mkLeaf declaredL) declaredN) (embedL l)
+        eitherToDhall (Right (n, e)) = App (App (App (App mkLeaf declaredL) declaredN) (embedN n)) e
+        InputType embedL declaredL = injectWith options
+        InputType embedN declaredN = injectWith options
