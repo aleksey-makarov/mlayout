ad143b229f82a213444c30e85396c2c19d5ab900
Aleksey Makarov <aleksey.makarov@gmail.com>
2019-07-16 11:41:41 +0300

    main: get rid of multiple input files feature

--- mlayout.hs
+++ mlayout.hs
@@ -6,23 +6,17 @@
 module Main (main) where
 
 import           Control.Monad.IO.Class
-import           Data.Text
--- import           Data.Text.Lazy.Encoding
 import           Data.Text.Prettyprint.Doc
 import           Data.Text.Prettyprint.Doc.Render.Text
--- import           Data.Time.LocalTime
--- import           Data.Time.RFC822
 import           Options.Applicative
 import           System.Exit
-import           System.FilePath
-import           System.Posix.Files
 import           System.IO
 import qualified Text.PrettyPrint.ANSI.Leijen as TPP
 import qualified Text.Trifecta.Parser as TRI
 import qualified Text.Trifecta.Result as TRI
 
 import qualified MLayout.Parser as MLP
-import qualified MLayout.Resolver as MLR
+-- import qualified MLayout.Resolver as MLR
 
 data OutputType
     = Pretty
@@ -30,34 +24,20 @@ data OutputType
     | Format FilePath
     deriving Show
 
-data InOutSpecification
-    = SingleInputFile
-        { inFileOpt       :: FilePath
-        , outFileOrDirOpt :: Maybe FilePath
-        }
---    | ManyInputFiles
---        { outDirOpt    :: FilePath
---        , outSuffixOpt :: Maybe String
---        , inFilesOpt   :: [FilePath]
---        }
-    deriving Show
-
 data MLayoutOptions
     = MLayoutOptions
         { outputTypeOpt :: OutputType
-        , inOutOpt  :: InOutSpecification
+        , inFile  :: FilePath
+        , outFile :: Maybe FilePath
         }
 
--- FIXME: add an option that sets base directory so that all the .layout files
--- under it will be formatted
--- FIXME: "-d -" should specify output to stdout
 optsParser :: Parser MLayoutOptions
 optsParser = MLayoutOptions
     <$> outputTypeParser
-    <*> inOutSpecParser
+    <*> inFileParser
+    <*> outFileParser
         where
             outputTypeParser = prettyOutputParser <|> prettyResolvedOutputParser <|> formatOutputParser
-            inOutSpecParser = singleInputFileParser -- <|> manyInputFilesParser
             prettyOutputParser = flag' Pretty
                 (  long "pretty"
                 <> short 'p'
@@ -74,132 +54,43 @@ optsParser = MLayoutOptions
                 <> metavar "TEMPLATE"
                 <> help "Format with template file"
                 )
-            singleInputFileParser = SingleInputFile
-                <$> argument str
+            inFileParser = argument str
                     (  metavar "INPUT_FILE"
                     <> help "Input file"
                     )
-                <*> optional (argument str
+            outFileParser = optional (argument str
                     (  metavar "OUTPUT_FILE_OR_DIR"
                     <> help "Output file or directory"
                     ))
---            manyInputFilesParser = ManyInputFiles
---                <$> strOption
---                    (  long "output-dir"
---                    <> short 'd'
---                    <> metavar "OUTPUT_DIR"
---                    <> help "Output directory"
---                    )
---                <*> optional (strOption
---                    (  long "suffix"
---                    <> short 's'
---                    <> metavar "SUFFIX"
---                    <> help "Output file suffix"
---                    ))
---                <*> some (argument str
---                    (  metavar "FILES..."
---                    <> help "Input files"
---                    ))
 
 opts :: ParserInfo MLayoutOptions
 opts = info (helper <*> optsParser)
     (  fullDesc
     <> header "mlayout - transform memory layout files"
     <> progDesc
-        (  "Transform input files in MLayout format into pretty printed MLayout files, "
-        ++ "or TEMPLATE files processed by dhall engine. "
-        ++ "The utility reads INPUT_FILE, processes it and outputs the result to OUTPUT_FILE_OR_DIR. "
---        ++ "If OUTPUT_DIR is specified, all FILES will be processed at one pass and written to files in that directory. "
---        ++ "The resulting files will have the same basename with suffix \'.mlayout\' changed to SUFFIX."
-        ++ "ID_STRING replaces generated id string"
+        (  "Transform input files in MLayout format into pretty printed MLayout files, \
+           \or TEMPLATE files processed by dhall engine. \
+           \The utility reads INPUT_FILE, processes it and outputs the result \
+           \to OUTPUT_FILE or to stdout."
         )
     )
 
-type WithFile = (Handle -> IO ()) -> IO ()
-
-data Task
-    = Task
-        { inFile :: FilePath
-        , withOutFile :: WithFile
-        }
-
-withOutFileStdout :: WithFile
-withOutFileStdout f = f stdout
-
-withOutFilePath :: FilePath -> WithFile
-withOutFilePath path f = withFile path WriteMode f
-
--- FIXME: add \n to the end of output both to prettyPrint and printJSON
-prettyPrint :: Pretty p => WithFile -> [p] -> IO ()
-prettyPrint withFile' layout = withFile' f
-    where
-        f :: Handle -> IO ()
-        f h = hPutDoc h $ vcat $ fmap pretty layout
-
-fileExistsAndIsDir :: FilePath -> IO Bool
-fileExistsAndIsDir f = do
-    e <- fileExist f
-    if not e
-        then return False
-        else isDirectory <$> getFileStatus f
+prettyPrint :: Pretty p => [p] -> Handle -> IO ()
+prettyPrint layout h = hPutDoc h $ vcat $ fmap pretty layout
 
 mlayout :: MLayoutOptions -> IO ()
-mlayout MLayoutOptions {..} = do
-
-    -- print outputTypeOpt
-    -- print inOutOpt
-
-    -- let
-    --    mkIdString :: IO Text
-    --    mkIdString = formatTimeRFC822 <$> getZonedTime
-
-    -- idString <- maybe mkIdString return idStringOpt
-
-    let
-        prepareOuputAction :: IO (FilePath -> WithFile -> [MLP.MLayout] -> IO ())
-        prepareOuputAction = case outputTypeOpt of
-            Pretty                   -> return (\ _ -> prettyPrint)
-            PrettyResolved           -> undefined -- @m
-            Format _templateFileName -> undefined
-
-        parseFile :: FilePath -> IO [MLP.MLayout]
-        parseFile inFile = TRI.parseFromFileEx MLP.parser inFile >>= \ case
+mlayout MLayoutOptions {..} = case outputTypeOpt of
+    Pretty -> do
+        parsed <- TRI.parseFromFileEx MLP.parser inFile >>= \ case
             TRI.Success ok -> return ok
             TRI.Failure xs  -> do
                 liftIO $ TPP.displayIO stderr $ TPP.renderPretty 0.8 80 $ (TRI._errDoc xs) <> TPP.linebreak
                 exitWith $ ExitFailure 1
+        maybe (prettyPrint parsed stdout) (\ x -> withFile x WriteMode $ prettyPrint parsed) outFile
 
-        outSuffix :: String
-        outSuffix = case outputTypeOpt of
-            Pretty          -> "mlayout"
-            PrettyResolved  -> "mlayout"
-            Format template -> takeBaseName template
+    PrettyResolved -> undefined
+    Format _ -> undefined
 
-        prepareBatch :: IO [Task]
-        prepareBatch = case inOutOpt of
-            -- return makes a singleton list, it's the same as (:[]) here
-            -- ManyInputFiles {..} -> undefined
-            SingleInputFile {..} -> (return . Task inFileOpt) <$> outFile
-                where
-                    outFile :: IO WithFile
-                    outFile = case outFileOrDirOpt of
-                        Just outFileOrDir -> do
-                            dir <- fileExistsAndIsDir outFileOrDir
-                            return $ withOutFilePath $ if dir
-                                then outFileOrDir </> (takeBaseName inFileOpt) <.> outSuffix
-                                else outFileOrDir
-                        Nothing -> return withOutFileStdout
-
-    outputAction <- prepareOuputAction
-
-    let
-        doTask Task {..} = do
-            parsed <- parseFile inFile
-            outputAction inFile withOutFile parsed
-
-    prepareBatch >>= mapM_ doTask
-
-    exitWith ExitSuccess
 
 main :: IO ()
 main = execParser opts >>= mlayout
