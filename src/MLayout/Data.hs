f0667b4c9e50bc24c734e6e7ee21093d7740e57e
Aleksei Makarov <aleksei.makarov@opensynergy.com>
2022-12-30 12:51:26 +0100

    Introducing the final data types, wip

--- src/MLayout/Data.hs
+++ src/MLayout/Data.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 {-# OPTIONS_GHC -Wall #-}
 
@@ -39,47 +40,99 @@ data ValueItem = ValueItem Integer Text Text -- =value name doc
 instance Pretty ValueItem where
     pretty (ValueItem v n d) = "=" <> pretty v <+> pretty n <+> dquotes (pretty d)
 
-{-
-
-data MemoryItemP
+data MemoryItem
     = MemoryItemMemory MLayoutMemory
     | MemoryItemWord MLayoutWord
 
-instance Pretty MemoryItemP where
+instance Pretty MemoryItem where
     pretty (MemoryItemMemory i) = pretty i
     pretty (MemoryItemWord i)   = pretty i
 
-data WordItemParsed
+data WordItem
     = WordItemWord MLayoutWord
     | WordItemBits MLayoutBits
     | WordItemValue ValueItem
 
-instance Pretty WordItemParsed where
+instance Pretty WordItem where
     pretty (WordItemWord i)  = pretty i
     pretty (WordItemBits i)  = pretty i
     pretty (WordItemValue i) = pretty i
 
-data BitsItemParsed
+data BitsItem
     = BitsItemBits MLayoutBits
     | BitsItemValue ValueItem
 
-instance Pretty BitsItemParsed where
+instance Pretty BitsItem where
     pretty (BitsItemBits i)  = pretty i
     pretty (BitsItemValue i) = pretty i
 
-data MLayoutMemory = MLayoutMemory LocationMB Text Text [MemoryItemP]
+data MLayoutMemory = MLayoutMemory
+    { mStart :: Start
+    , mWidth :: Word
+    , mName  :: Text
+    , mDoc   :: Text
+    , mItems :: [MemoryItem]
+    }
 
 instance Pretty MLayoutMemory where
-    pretty (MLayoutMemory l n d mis) = PP.brackets (prettyLocationM l) <+> pretty n <> prettyDoc d <> prettySubitems mis
+    pretty MLayoutMemory { .. } = brackets (prettyLocation mStart mWidth) <+> pretty mName <> prettyDoc mDoc <> prettySubitems mItems
+        where
+            prettyLocation (Start start)           width = pretty width <> "@" <> pretty start
+            prettyLocation (Fields nel)            width = undefined
+            prettyLocation (Periodic start n step) width = undefined
 
-data MLayoutWord = MLayoutWord LocationW Text Text [WordItemParsed]
+--    pretty (FieldsP pairs) = PP.braces $ cat $ punctuate ", " $ NEL.toList $ fmap posPretty pairs
+--        where
+--            posPretty (mat, name) = maybe mempty ((<> PP.space) . pretty) mat <> pretty name
+--    pretty (PeriodicP mat n ms) = prettyMaybe mat <> PP.brackets (pretty n <> maybe mempty appendStep ms)
+--        where
+--            appendStep w = PP.space <> "+" <> pretty w
+
+
+-- prettyLocationM :: LocationMB -> Doc ann
+-- prettyLocationM (FromToP mwFrom mwTo) = prettyMaybe mwFrom <> ":" <> prettyMaybe mwTo
+-- prettyLocationM (WidthStartP Nothing NextP) = mempty
+-- prettyLocationM (WidthStartP mw NextP) = prettyMaybe mw
+-- prettyLocationM (WidthStartP mw sp) = prettyMaybe mw <> "@" <> pretty sp
+
+data MLayoutWord = MLayoutWord
+    { wStart :: Start
+    , wWidth :: Word
+    , wName  :: Text
+    , wDoc   :: Text
+    , wItems :: [MemoryItem]
+    }
 
 instance Pretty MLayoutWord where
-    pretty (MLayoutWord l n d wis) = PP.brackets (prettyLocationW l) <+> pretty n <> prettyDoc d <> prettySubitems wis
+    pretty MLayoutWord { .. } = undefined
 
-data MLayoutBits = MLayoutBits LocationMB Text Text [BitsItemParsed]
+data MLayoutBits = MLayoutBits
+    { bStart :: Start
+    , bWidth :: Word
+    , bName  :: Text
+    , bDoc   :: Text
+    , bItems :: [MemoryItem]
+    }
 
 instance Pretty MLayoutBits where
-    pretty (MLayoutBits l n d bis) = PP.angles (prettyLocationB l) <+> pretty n <> prettyDoc d <> prettySubitems bis
+    pretty MLayoutBits { .. } = undefined
 
--}
+prettyDoc :: Text -> Doc ann
+prettyDoc t = if Data.Text.null t
+                  then mempty
+                  else space <> dquotes (pretty t)
+
+prettySubitems :: Pretty c => [c] -> Doc ann
+prettySubitems [] = mempty
+prettySubitems is = space <> braces (line <> indent 4 (vsep $ fmap pretty is) <> line)
+
+--
+-- data MLayoutWord = MLayoutWord LocationW Text Text [WordItemParsed]
+--
+-- instance Pretty MLayoutWord where
+--     pretty (MLayoutWord l n d wis) = PP.brackets (prettyLocationW l) <+> pretty n <> prettyDoc d <> prettySubitems wis
+--
+-- data MLayoutBits = MLayoutBits LocationMB Text Text [BitsItemParsed]
+--
+-- instance Pretty MLayoutBits where
+--     pretty (MLayoutBits l n d bis) = PP.angles (prettyLocationB l) <+> pretty n <> prettyDoc d <> prettySubitems bis
